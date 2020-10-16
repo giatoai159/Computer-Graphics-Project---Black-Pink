56f7d746433115864f327998f500920374cffa3a
Huỳnh Gia Toại <giatoai159@gmail.com>
2020-10-16 17:33:44 +0700

    Test some more

--- Source/main.py
+++ Source/main.py
@@ -1,9 +1,13 @@
 from packages import *
 
-display = (800, 600)
+display = (1280, 720)
 icon = ""
 name = "Black Pink"
 timer = pygame.time.Clock()
+
+# Color constant
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 
 def quit_game():
@@ -11,34 +15,43 @@ def quit_game():
     sys.exit()
 
 
-def main_menu():
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                quit_game()
-        keys_flag = pygame.key.get_pressed()
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        glClearColor(1, 1, 1, 1)
-        pygame.display.flip()
-
-def game_init():
-    # PyGame initialization
+def main():
+    # PyGame Initialization
     pygame.init()
     while not pygame.get_init():
         print("PyGame Initialization Failed.")
-    # Game window properties
-    game_window = pygame.display.set_mode(display, DOUBLEBUF | OPENGL)  # Set game width and height
+    game_window = pygame.display.set_mode(display, DOUBLEBUF | OPENGL | pygame.RESIZABLE)  # Set game width and height
     pygame.display.set_caption(name)  # Set game title
     # _icon = pygame.image.load(icon)  # Load icon
     # pygame.display.set_icon(_icon)  # Set game icon
-    # Game main loop
-    main_menu()
+    fullscreen = False  # Variable for fullscreen
+    monitor_size = [pygame.display.Info().current_w, pygame.display.Info().current_h]
 
-
-
-def main():
-    game_init()
+    while True:
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glClearColor(1, 0.5, 0.7, 1)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                quit_game()
+            if event.type == KEYDOWN:
+                if event.key == K_f:
+                    fullscreen = not fullscreen
+                    if fullscreen:
+                        game_window = pygame.display.set_mode(monitor_size, DOUBLEBUF | OPENGL | pygame.FULLSCREEN)
+                    else:
+                        game_window = pygame.display.set_mode((game_window.get_width(), game_window.get_height()),
+                                                              DOUBLEBUF | OPENGL | pygame.RESIZABLE)
+        glBegin(GL_QUADS)
+        glColor3f(1, 0, 0)
+        glVertex2f(-0.1, -0.1)
+        glVertex2f(0.2, -0.1)
+        glVertex2f(0.2, 0.3)
+        glVertex2f(-0.1, 0.3)
+        glEnd()
+        pygame.display.flip()
 
 
 main()
+
+
 pygame.quit()
--- Source/packages.py
+++ Source/packages.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import time
 from OpenGL.GL import *
 from pygame.locals import *
 

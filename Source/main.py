ae8c8a85bd6c738ac177db27e96085e0feabcdb9
Huỳnh Gia Toại <giatoai159@gmail.com>
2020-10-21 23:11:37 +0700

    Send help

--- Source/main.py
+++ Source/main.py
@@ -1,13 +1,73 @@
-from packages import *
+import pygame
+import sys
+import numpy as np
+from OpenGL.GL import *
+from OpenGL.GL.shaders import compileShader, compileProgram
+from pygame.locals import *
 
 display = (1280, 720)
 icon = ""
-name = "Black Pink"
+name = "Test"
 timer = pygame.time.Clock()
+shader_program = None
+VAO = None
+VBO = None
 
-# Color constant
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+# Creating a vertex shader
+vertex_shader_code = """
+#version 330
+
+layout (location = 0) in vec3 pos;
+
+void main()
+{
+    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
+}
+"""
+
+fragment_shader_code = """
+#version 330
+
+out vec4 color;
+
+void main()
+{
+    color = vec4(1.0, 0.0, 0.0, 1.0);
+}
+"""
+
+
+def shader_compile():
+    global shader_program
+
+    shader_program = compileProgram(compileShader(vertex_shader_code, GL_VERTEX_SHADER),
+                                    compileShader(fragment_shader_code, GL_FRAGMENT_SHADER))
+
+
+def create_triangle():
+    global VAO
+    global VBO
+    vertices = [
+        -0.5, -0.5, 0.0,
+        0.5, -0.5, 0.0,
+        0.0, 0.5, 0.0
+    ]
+    vertices = np.array(vertices, dtype=np.float32)
+
+    VAO = glGenVertexArrays(1)
+    glBindVertexArray(VAO)
+
+    VBO = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+
+    position = glGetAttribLocation(shader_program, 'pos')
+    glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 0, None)
+    glEnableVertexAttribArray(position)
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0)
+
+    glBindVertexArray(0)
 
 
 def quit_game():
@@ -24,34 +84,30 @@ def main():
     pygame.display.set_caption(name)  # Set game title
     # _icon = pygame.image.load(icon)  # Load icon
     # pygame.display.set_icon(_icon)  # Set game icon
-    fullscreen = False  # Variable for fullscreen
-    monitor_size = [pygame.display.Info().current_w, pygame.display.Info().current_h]
+    # glViewport(0, 0, display[0], display[1])
+
+    shader_compile()
+    create_triangle()
 
     while True:
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glClearColor(1, 0.5, 0.7, 1)
+        glClearColor(1, 1, 1, 1)
         for event in pygame.event.get():
             if event.type == QUIT:
                 quit_game()
-            if event.type == KEYDOWN:
-                if event.key == K_f:
-                    fullscreen = not fullscreen
-                    if fullscreen:
-                        game_window = pygame.display.set_mode(monitor_size, DOUBLEBUF | OPENGL | pygame.FULLSCREEN)
-                    else:
-                        game_window = pygame.display.set_mode((game_window.get_width(), game_window.get_height()),
-                                                              DOUBLEBUF | OPENGL | pygame.RESIZABLE)
-        glBegin(GL_QUADS)
-        glColor3f(1, 0, 0)
-        glVertex2f(-0.1, -0.1)
-        glVertex2f(0.2, -0.1)
-        glVertex2f(0.2, 0.3)
-        glVertex2f(-0.1, 0.3)
-        glEnd()
-        pygame.display.flip()
+            if event.type == VIDEORESIZE:
+                glViewport(0, 0, event.w, event.h)
+        glUseProgram(shader_program)
+        glBindVertexArray(VAO)
+        glDrawArrays(GL_TRIANGLES, 0, 3)
+        glUseProgram(0)
+        glBindVertexArray(0)
+
+        pygame.display.flip()  # = glfw.swap_buffers(window)
 
 
-main()
+if __name__ == '__main__':
 
+    main()
 
 pygame.quit()
--- Source/packages.py
+++
@@ -1,7 +0,0 @@
-import pygame
-import sys
-import time
-from OpenGL.GL import *
-from pygame.locals import *
-
-
